c52f5561e8add628da4ee729b527137d0749d375
PhilipBerg <pberg@live.se>
2021-06-17 16:58:52 -0500

    Initial commit

--- R/fit_gamma_regressions.R
+++ R/fit_gamma_regressions.R
@@ -1,10 +1,17 @@
-#' Function for fitting the mean-variance gamma regression
+#' Function for Fitting the Mean-Variance Gamma Regression Models
 #'
-#' @param data
-#' @param design
-#' @param id_col
+#'`fit_gamma_regressions` calls both \code{\link[pair]{fit_gamma_regression}}
+#'    and \code{\link[pair]{fit_gamma_weights}} and returns a list containing
+#'    the models for imputation in `$imputation` and the weights in `$weights`.
 #'
-#' @return
+#' @param data a `data.frame` to calculate the weights for
+#' @param design a design or model matrix
+#' @param id_col a character for the name of the column containing the
+#'     name of the features in data (e.g., peptides, proteins, etc.)
+#'
+#' @return a list with the gamma regressions for imputation in
+#'         `$imputation$<condition>` and the gamma regression for generating
+#'         precision weights in `$weights`.
 #' @export
 #'
 #' @importFrom dplyr %>%
@@ -13,16 +20,52 @@
 #' @examples
 utils::globalVariables(c(".", "sd", "model"))
 fit_gamma_regressions <- function(data, design, id_col = "id") {
-  gamma_reg <- data %>%
+  gamma_reg_imp <- data %>%
     fit_gamma_imputation(design, id_col)
-  gamma_reg_all <- data %>%
-
+  gamma_reg_w <- data %>%
+    fit_gamma_weights(design, id_col)
   return(
     list(
-      all = gamma_reg_all,
-      individual = gamma_reg
+      weights = gamma_reg_w,
+      imputation = gamma_reg_imp
     )
   )
+}
+
+#' Title
+#'
+#' @param data
+#' @param design
+#' @param id_col
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fit_gamma_imputation <- function(data, design, id_col = "id") {
+  data %>%
+    prep_data_for_gamma_imputation_regression(design, id_col) %>%
+    dplyr::group_by(name) %>%
+    fit_gamma_model() %>%
+    split.data.frame(.$name) %>%
+    extract_model()
+}
+
+#' Title
+#'
+#' @param data
+#' @param design
+#' @param id_col
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fit_gamma_weights <- function(data, design, id_col = "id"){
+  data %>%
+    prep_data_for_gamma_weight_regression(design, id_col) %>%
+    fit_gamma_model() %>%
+    extract_model()
 }
 
 prep_data_for_gamma_weight_regression <- function(data, design, id_col = "id") {
@@ -75,49 +118,20 @@ pivot_data_for_gamma_regression <- function(data, design) {
     tidyr::pivot_longer(dplyr::matches(conditions))
 }
 
-#' Title
-#'
-#' @param data
-#' @param design
-#' @param id_col
-#'
-#' @return
-#' @export
-#'
-#' @examples
-fit_gamma_imputation <- function(data, design, id_col = "id") {
-  data %>%
-    prep_data_for_gamma_imputation_regression(design, id_col) %>%
-    fit_gamma_model() %>%
-    split.data.frame(.$name) %>%
-    extract_model()
-}
-
-#' Title
-#'
-#' @param data
-#' @param design
-#' @param id_col
-#'
-#' @return
-#' @export
-#'
-#' @examples
-fit_gamma_weights <- function(data, design, id_col = "id"){
-  data %>%
-    prep_data_for_gamma_weight_regression(design, id_col) %>%
-    fit_gamma_model() %>%
-    extract_model()
-}
-
 extract_model <- function(data) {
-  if (nrow(data)>1) {
+  if (is.data.frame(data)) {
+    data %>%
+      magrittr::use_series(model) %>%
+      magrittr::extract2(1)
+  }else if(is.list(data)){
     data %>%
       purrr::map(magrittr::use_series, model) %>%
       purrr::map(magrittr::extract2, 1)
   }else {
-    data %>%
-      magrittr::use_series(model) %>%
-      magrittr::extract2(1)
+    error_message <- paste('Data of class',
+                           paste0(class(data), collapse = ', '),
+                           'not suppported.'
+    )
+    rlang::abort(error_message)
   }
 }
